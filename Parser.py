b1e6222b0d260888abfe429a21ba7030e3cbe0a8
albertojc6 <133217246+albertojc6@users.noreply.github.com>
2024-03-10 18:36:11 +0100

    Parser

--- Parser.py
+++ Parser.py
@@ -1,47 +1,38 @@
 import numpy as np
 
 def parse(path):
+    """
+    Function to read optimization problem data from a file
+
+    Pre-conditions: path is a valid path to a readable file. Problem's parameters are written in the following lines
+                    of pattern '<parameter>=', having parameter = {c, A, b}. Only numbers must be written.
+
+    Post-condition: creates from data in path a vector with cost coefficients c, antoher with right-hand side of 
+                    constraints b, and a matrix with coefficients of variables for each constraint A
+    """
+    
     with open(path, 'r') as file:
 
         lines = [line.strip() for line in file.readlines() if line.strip()]
 
-    # Variables to store the parsed data
+    # Dict to store the parsed data
     variables = {}
 
-    # Read line by line
     current_variable = None
     for line in lines:
+
         if line.endswith('='):
-            current_variable = line[:-1]  # What is the variable name?
+            # Get variable name and add to dict
+            current_variable = line[:-1]  
             variables[current_variable] = []    
+
         elif current_variable is not None:
-            # Extract and store in variable
+            # Extract and store in variable key dict
             values = [int(val) for val in line.split()]
             if current_variable == 'A':
                 variables[current_variable].append(values)
             else:
                 variables[current_variable].extend(values)
 
-
     return np.array(variables['c']), np.matrix(variables['A']), np.array(variables['b'])
-
-c, A, b = parse('Datos/Datos5_2.txt')
-
-'''theta = float('inf')
-        for i in negative_directions.sort():
-            quocient =  -self._x_B[i] / d_B[i]
-            if quocient < theta:
-                theta = quocient
-                p = i'''
-
-'''candidates = -self._x_B[negative_directions] / d_B[negative_directions]
-        min_value = np.min(candidates)
-        if np.count_nonzero(candidates == min_value)>=2:
-            self._theta = np.min(candidates)
-            self._p = np.argmin(self._i_B[negative_directions])
-    
-        else:
-
-            self._theta, self._p = np.min(candidates), negative_directions[np.argmin(candidates)]'''
-
 
