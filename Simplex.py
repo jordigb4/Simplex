3750146d4919d710edd6a0539e9fa7ed776dfef9
albertojc6 <133217246+albertojc6@users.noreply.github.com>
2024-03-15 13:17:24 +0100

    update_inverse

--- Simplex.py
+++ Simplex.py
@@ -4,34 +4,17 @@ from Parser import parse
 
 class Simplex:
 
-    def __init__(self, c, A, b, fase1 = False) -> None:
+    def __init__(self, c, A, b, fase1 = False, debug_file = 'debug') -> None:    
 
         self.__fase1 = fase1
         self._m = A.shape[0]; self._n = A.shape[1]
         self._c = c
         self._A = A
         self._b = b
-        self.__iteration =0
+        self._debug_file = debug_file
+        self._iteration = 0
 
     def _isoptimal(self):
-
-        with open("debug.txt", "a") as debug_file:
-            debug_file.write(f"Simplex {'fase 1' if self.__fase1 else 'primal' }. Iteració: {self.__iteration}\n\n")
-            debug_file.write(f"x_B = {self._x_B}\n")
-            if hasattr(self, '_d_B'):
-                debug_file.write(f"direccions = {self._d_B}\n")
-            if hasattr(self, '_r'):
-                debug_file.write(f"reduits = {self._r}\n")
-            if hasattr(self, '_p'):
-                debug_file.write(f"p = {self._p}\n")
-            if hasattr(self, '_q'):
-                debug_file.write(f"q = {self._q}\n")
-            debug_file.write(f"i_B = {self._i_B}\n")
-            debug_file.write(f"i_N= {self._i_N}\n")
-
-            debug_file.write("====================================================\n\n")
-
-        self.__iteration +=1
 
         self._r = self._c_N - (self._c_B @ (self._B_inv @ self._A_N))
         self._r = np.asarray(self._r).reshape(-1)
@@ -82,6 +65,7 @@ class Simplex:
 
         self._z += self._theta * self._r[self._q]
 
+
     def _update_inverse(self):
         
         E = np.eye(self._m)
@@ -103,9 +87,11 @@ class Simplex:
         #1. Find initial Basic Feasible Solution
         if not self.__fase1:
             self._i_B, self._B_inv = self._fase_1()
+            with open(f"{self._debug_file}.txt", "a") as debug_file:
+                debug_file.write('Fase II \n')
             self._B = self._A[:,self._i_B]
             self._i_N = sorted(np.setdiff1d(np.arange(self._n), self._i_B))
-        else:
+        else:    
             self._i_N, self._i_B = np.arange(0 , self._n - self._m), np.arange(self._n - self._m, self._n)
             self._B = self._B_inv = np.eye(self._m)
 
@@ -126,8 +112,13 @@ class Simplex:
             self._get_theta()
             #5. Update structures
             self._update_values()
-            
+
+            self._iteration +=1
+            with open(f"{self._debug_file}.txt", "a") as debug_file:
+                debug_file.write(f'Iteració {self._iteration}: p = {self._p}, q = {self._q}, B(p) = {self._i_B[self._p]}, theta* = {np.round(self._theta, 4)}, z = {np.round(self._z, 4)} \n')
+
         if self.__fase1:
+
             if np.isclose(self._z, 0, atol=1e-10):
                 while self._isoptimal() and not self._clean_phase1():
                     candidates = np.where(self._r == 0)[0]
@@ -135,24 +126,43 @@ class Simplex:
                     self._get_direction()
                     self._get_theta()
                     self._update_values()
+                    self._iteration +=1
+                with open(f"{self._debug_file}.txt", "a") as debug_file:
+                debug_file.write(f'Iteració {self._iteration}: p = {self._p}, q = {self._q}, B(p) = {self._i_B[self._p]}, theta* = {np.round(self._theta, 4)}, z = {np.round(self._z, 4)} \n')
+                with open(f"{self._debug_file}.txt", "a") as debug_file:
+                    debug_file.write(f'Solució bàsica factible trobada, iteració {self._iteration} \n')
                 return self._i_B, self._B_inv
             
             #artificial variables take value at optimal
             raise Exception("The Problem is not feasible")
-        print(self._z)
-        print(f"Optimal Solution: {self._x_B}")
-                     
-    def _fase_1(self):
         
+        with open(f"{self._debug_file}.txt", "a") as debug_file:
+                    debug_file.write(f'Solució òptima trobada, iteració {self._iteration}, z = {self._z} \n')
+                    debug_file.write('Fi simplex primal \n\n')
+                    debug_file.write('Solució òptima: \n\n')
+                    debug_file.write(f'vb = {self._i_B} \n')
+                    debug_file.write(f'xb = {np.round(self._x_B, 3)} \n')
+                    debug_file.write(f'z = {self._z} \n')
+                    debug_file.write(f'r = {np.round(self._r, 2)}')
+
+    def _fase_1(self):
+
+        with open(f"{self._debug_file}.txt", 'w') as file:
+            pass #cleaning the file
+        with open(f"{self._debug_file}.txt", "a") as debug_file:
+
+            debug_file.write('Inici simplex primal amb regla de Bland \n')
+            debug_file.write('Fase I\n')
+
         c = np.append(np.zeros(self._n, dtype=int), np.ones(self._m, dtype=int))
         identity_matrix = np.eye(self._m)
         A = np.hstack((self._A, identity_matrix))
         b = self._b
         
-        fase1 =  Simplex(c, A, b, fase1 = True)
+        fase1 =  Simplex(c, A, b, fase1 = True, debug_file = self._debug_file)
         i_b, B_inv = fase1.solve()
         return i_b, B_inv
         
-c, A, b = parse('Datos/Datos23_1.txt')
-sim = Simplex(c, A, b)
+c, A, b = parse('Datos/Datos5_1.txt')
+sim = Simplex(c, A, b, debug_file='Debug_Datos_5_1')
 sim.solve()
